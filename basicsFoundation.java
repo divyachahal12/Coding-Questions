6fd823f05eed47531c0856f787d419d7c88bdd01
divyachahal12 <66896948+divyachahal12@users.noreply.github.com>
2021-07-30 00:04:53 +0530

    Update basicsFoundation.java

--- basicsFoundation.java
+++ basicsFoundation.java
@@ -8,3 +8,43 @@ public class Main {
 
     }
 }
+
+//print equilateral triangle
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("  *\n * *\n* * *");
+    }
+}
+
+//grading system IF-ELSE 
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      // input - don't change this code
+      Scanner scn = new Scanner(System.in);
+      int marks = scn.nextInt();
+      // input - don't change this code
+      
+      if(marks > 90)
+        System.out.println("excellent");
+        
+      else if(marks > 80 && marks <= 90)
+        System.out.println("good");
+        
+      else if(marks > 70 && marks <= 80)
+        System.out.println("fair");
+        
+      else if(marks > 60 && marks <= 70)
+        System.out.println("meets expectations");
+        
+      else
+        System.out.println("below par");
+  
+   }
+  }
+
+//
