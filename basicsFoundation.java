2bcb485c44238c51da8ebe07ad8e89b711bc2651
divyachahal12 <66896948+divyachahal12@users.noreply.github.com>
2021-07-31 00:14:35 +0530

    Update basicsFoundation.java

--- basicsFoundation.java
+++ basicsFoundation.java
@@ -47,4 +47,24 @@ import java.util.*;
    }
   }
 
-//
+//Quiz Question *************************************************************************************************
+public class Main{
+    public static void main(String[] args){
+    int i = 10;
+    if(i++ == i)
+ 	 System.out.println(i + " is good");
+    else
+ 	 System.out.println(i + " is bad");
+ 
+    int j = 20;
+    if(++j == j)
+ 	 System.out.println(j + " is good");
+    else
+ 	 System.out.println(j + " is bad");
+    } 
+}
+
+//OUTPUT
+//11 is bad
+//21 is good
+
