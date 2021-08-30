85a2233f148b9909a4fb7197a2586c4cbfe8a30b
divyachahal12 <66896948+divyachahal12@users.noreply.github.com>
2021-08-30 22:56:51 +0530

    Update and rename TreeLevelUp.java to Tree Level Up.java

---
+++ Tree Level Up.java
@@ -0,0 +1,88 @@
+//making our tree class
+public static class TreeNode(){
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    
+    TreeNode(int val){
+        this.val = val;
+    }
+    
+    
+    //size of tree = number of nodes in tree
+    public static int size(TreeNode root){
+        // if(root == null)
+        //     return 0;
+        // int ans = size(root.left) + size(root.right) + 1;
+        // return ans;
+        
+        //is same as below line
+        
+        return root == null ? 0 :  size(root.left) + size(root.right) + 1;
+    }
+    
+    
+    //height or depth of a tree is same, seen from different perspectives
+    public static int height(TreeNode root){
+        // if(root == null)
+        //     return -1;
+        // int ans =  Math.max(height(root.left), height(root.right))+1;  
+        // we have faith that left and right childs will give their max heights, and 
+        // total height of tree would be max of left child or right child + 1(for root itself)
+        // return ans;
+        
+        //is same as below line
+        return root == null ? -1 :  Math.max(height(root.left), height(root.right))+1;
+    }
+    
+    
+    //max value node in tree
+    public static int maxValueNode(TreeNode root){
+        // if(root == null)
+        //     return -(int)1e9;
+        // int ans = Math.max(root.val, Math.max(maxValueNode(root.left), maxValueNode(root.right)));
+        // return ans;
+        
+        //is same as below line
+        
+        return root == null ? -(int)1e9 :  Math.max(root.val, Math.max(maxValueNode(root.left), maxValueNode(root.right)));
+    }
+    
+    //min value node in tree
+    public static int minValueNode(TreeNode root){
+        // if(root == null)
+        //     return (int)1e9;
+        // int ans = Math.min(root.val, Math.min(minValueNode(root.left), minValueNode(root.right)));
+        // return ans;
+        
+        //is same as below line
+        
+        return root == null ? (int)1e9 :  Math.min(root.val, Math.min(minValueNode(root.left), minValueNode(root.right)));
+    }
+    
+    //find if a given data value is present in tree or not
+    public static boolean find(TreeNode root, int data){
+        if(root == null)
+            return false;
+        if(root.val == data)
+            return true;
+        
+        return find(root.left, data) || find(root.right, data);// || operator if any one place true is returned then compiler wont check further condition
+    }
+    
+    //Node to Root Path
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
