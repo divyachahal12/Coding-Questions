24ef358af707eeaaee9cab89f60e00ff2464f495
divyachahal12 <66896948+divyachahal12@users.noreply.github.com>
2022-12-18 23:51:26 +0530

    Update LinkedListsFoundation.java

--- LinkedListsFoundation.java
+++ LinkedListsFoundation.java
@@ -37,4 +37,65 @@ public void display(){
       
  }
 
+//Remove First in LL
+// You are required to complete the body of removeFirst function 
+//          removeFirst - This function is required to remove the first element from 
+//           Linked List. Also, if there is only one element, this should set head and tail to 
+//           null. If there are no elements, this should print "List is empty".
+public void removeFirst(){
+        if(size == 0) {
+          System.out.println("List is empty");
+        }
+        else if(size == 1){
+            head = tail = null;
+            size = 0;
+        }else{
+            head = head.next;
+            size--;
+        }
+      
+    }
+  }
+
+//get valur in LL
+public int getFirst() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return head.data;
+      }
+    }
+
+    public int getLast() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return tail.data;
+      }
+    }
+
+    public int getAt(int idx) {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      }else if(idx < 0 || idx >= size){
+          System.out.println("Invalid arguments");
+          return -1;
+      } 
+      else {
+        Node temp = head;
+        for (int i = 0; i < idx; i++) {
+          temp = temp.next;
+        }
+        //or use while loop--|
+//         while(idx > 0){
+//             temp = temp.next;
+//             idx--;
+//         }
+        return temp.data;
+      }
+    }
+  }
 
