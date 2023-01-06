9d8b2ce51ab8d7227f4f90e0f0e994dd9c3cfd2b
divyachahal12 <66896948+divyachahal12@users.noreply.github.com>
2023-01-06 20:28:48 +0530

    Update TreesFoundation.java

--- TreesFoundation.java
+++ TreesFoundation.java
@@ -27,5 +27,40 @@ public static void construct(){
       }
   }
 
+//Display a Generic Tree
+/*
+  We have faith that root will print itself and it's children, 
+  and children will print themselves and their children
+  d(10) -> s(10) + d(20) + d(30) + d(40)
+  */
+  public static void display(Node node){
+      String str = node.data + " -> ";
+      for(Node child : node.children){
+          str += child.data + ",";
+      }
+      
+      str += ".";
+      System.out.println(str);
+      
+      for(Node child : node.children){
+          display(child);
+      }
+  }
+
+/*OUTPUT
+10 -> 20, 30, 40,. V
+20 -> 50, 60,. ~
+50->.
+60 -> .
+30-570,80, 90, V
+70 -> ."
+80->110, 120,. V
+110 ->.
+120 ->.
+90 -> .\
+40 -> 100,.
+100 ->.
+*/
+
 
   
