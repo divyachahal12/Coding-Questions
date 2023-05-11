949b9d1cf72dd27531f9bc65428b295472e0241e
divyachahal12 <66896948+divyachahal12@users.noreply.github.com>
2023-05-11 16:13:14 +0530

    Update graphsFoundation.java

--- graphsFoundation.java
+++ graphsFoundation.java
@@ -730,11 +730,90 @@ static class Pair{
       }
    }
 
-//
+//Is Graph Cyclic
 /*
+You are required to find and print if the graph is cyclic.
+Print true if there exists a cycle, else false.
+Note: Input is given in the form of adjacency list.
+Note: Graph may or may not be connected.
 
+What does it mean, when we say that there is a cycle present in the graph?
+It means that there exists a pair of vertices (u, v) such that there is not an unique path from node u to node v. 
+In simple words, there exists at least two different paths of reaching node v from the node u.
+To find if there exists such two paths, we can do a BFS traversal of the given graph. For every visited vertex "v", we will push all the neighbouring vertices "u" into the queue.
+Now, here is the crux of the algorithm. 
+If the neighboring vertex is already visited, then there exists at least two different paths from node v to node u. 
+It is because we were able to visit the vertex u, as it must have been popped out from the queue earlier.
+Now, since there exists two different paths from node v to node u, there is a cycle present in the graph, (and nodes u and v must be part of that cycle).
+
+Time Complexity:
+We are doing a simple BFS traversal of the graph. Hence the time complexity will be O(N + E) where N = number of vertices and E = number of edges.
+
+Space Complexity:
+We are building a visited array and a parent array of size equal to the number of vertices. Also, we are taking the queue data structure to perform BFS traversal,
+which will store at max n nodes.
+Hence, the total space complexity will be O(n + n + n) = O(n) only.
+Please note that we are not taking into account the space taken to build the adjacency list, as it was given to us as an input.
 */
+      public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      // write your code here
+      boolean[] visited = new int[vtces];
+      for(int i = 0; i < vtces; i++){
+          if(visited[i] == false){
+              boolean cycle = isCyclic(graph, i, visited);
+              if(cycle == true){
+                  System.out.println("true");
+                  return;
+              }
+          }
+      }
+      System.out.println("false");
+   }
+   static class Pair{
+       int v;
+       String psf;
+       
+       Pair(int v, String psf){
+           this.v = v;
+           this.psf = psf;
+       }
+   }
+   public static boolean isCyclic(ArrayList<Edge> graph, int src, boolean[] visited){
+       ArrayDeque<Pair> q = new ArrayDeque<>();
+       q.add(new Pair(src, src + ""));
+       
+       while(q.size() > 0){
+           Pair rem = q.removeFirst();
+           if(visited[rem.v] == true){
+               return true;
+           }
+           visited[rem.v] = true;
+           for(Edge e : graph[rem.v]){
+               if(visited[e.nbr] == false){
+                    q.add(new Pair(e.nbr, rem.psf + e.nbr));
+               }
+           }
+       }
+       return false;
+   }
 
 
 
